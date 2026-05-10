b5ed17b7930fbb90c6ff82e2474fa94a53115f26
dgecer <dggecer@gmail.com>
2026-05-10 15:44:10 -0400

    Implemented adaptive scheduler logic

---
+++ src/scheduler.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <unistd.h>
+
+#include "scheduler.h"
+#include "workload.h"
+
+void run_scheduler(double attention_level)
+{
+    int quantum;
+
+    if (attention_level > 0.6)
+    {
+        quantum = 1;
+
+        printf("\n[MODE] HIGH ATTENTION\n");
+        printf("Aggressive scheduling enabled.\n");
+    }
+    else
+    {
+        quantum = 3;
+
+        printf("\n[MODE] LOW ATTENTION\n");
+        printf("Conservative scheduling enabled.\n");
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("\nRunning task %d with quantum %d...\n", i + 1, quantum);
+
+        heavy_task(i + 1);
+
+        sleep(quantum);
+    }
+}
